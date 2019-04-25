7888b38eb2f9e925bbd8ad20cba818ba301dbd7a
rusted-0-dev <rusted-0-dev@gmail.com>
2019-04-25 20:23:10 +0530

    file uploading complete

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import mul_ab, UploadExcel
+from .views import mul_ab, UploadExcel, downloadExcel
 
 urlpatterns = [
     path('mul/<int:a>/<int:b>/', mul_ab, name="mul_ab"),
-    path('uploadExcel/', UploadExcel, name="UploadExcel")
+    path('uploadExcel/', UploadExcel.as_view(), name="UploadExcel"),
+    path('downloadExcel/', downloadExcel)
 ]
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.views import View
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+# to get the FileField for the html. 
 from django import forms
+# to use the excel
+import django_excel as excel
+#
+import os
+from django.conf import settings
+
+# not used here...
+# decorator written on a class, says which method it should be used on. 
+from django.utils.decorators import method_decorator
+# to deal with missing csrf token on POST requests 
 from django.views.decorators.csrf import csrf_exempt
+
 
 
 from .models import InputRowModel, ExportRowModel
@@ -14,18 +26,62 @@ def mul_ab(request, **kwargs):
     data = {"mul": a*b}
     return JsonResponse(data)
 
-class UploadFileForm(forms.Form):
-    file = forms.FileField
 
-@csrf_exempt
+class UploadFileForm(forms.Form):
+    file = forms.FileField   
+
+
+# @method_decorator(csrf_exempt, name='dispatch')
 class UploadExcel(View):
 
-    def __init__(self, **kwargs):
-        print("----------------------------->here<---------------------------------")
-        return super().__init__(**kwargs)
-    
+    export_sheet = None
+      
     def post(self, request):
-        if (request.method != 'POST'):
-            return render(request, 'uploadFile.html', {'foo':'bar'})
-        else:
-            return render(request, 'uploadFile.html', {'foo':'bar'})
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            filehandle = request.FILES['input-excel']
+            return self.processExcel(request, form, filehandle)
+    
+    def get(self, request):
+        form = UploadFileForm()
+        return render(request, 'uploadFile.html', {
+                'title': 'File Upload Form',
+                'form': form,
+                'export_sheet': self.export_sheet
+            })
+
+    def processExcel(self, request, form, filehandle):
+        sheet = filehandle.get_sheet()
+        export_data = []
+        for row in sheet:
+            export_row = []
+            if row[0] == 'a':
+                export_row.append(row[0])
+                export_row.append(row[1])
+                export_row.append('sum')
+                export_row.append('product')
+                export_data.append(export_row)
+                continue
+            
+            export_row.append(int(row[0]))
+            export_row.append(int(row[1]))
+            export_row.append(row[0] + row[1])
+            export_row.append(row[0] * row[1])
+            export_data.append(export_row)
+        export_sheet = excel.pe.Sheet(export_data)
+        export_sheet.save_as('output-excel.xlsx')
+        return render(request, 'uploadFile.html', {
+            'title': 'File Upload Form',
+            'form': form,
+            'export_sheet': self.export_sheet
+        })
+          
+    # return excel.make_response(, file_type='xlsx', file_name='output-excel.xlsx')
+
+def downloadExcel(request):
+    file_name = 'output-excel.xslx'
+    print(settings.MEDIA_ROOT, 'asdfasdfasdf')
+    with open(file_name, 'rb') as fh:
+        response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+        response['Content-Disposition'] = 'inline; filename=' + file_name
+        return response
